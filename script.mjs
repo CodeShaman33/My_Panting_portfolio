dd0eb00c1639d57700ca78bcc6133d94b53e84ef
PsychoShaman <111572177+PsychoShaman@users.noreply.github.com>
2023-02-18 17:37:56 +0100

    create ul list with li elements generated by script based on data in array

--- script.mjs
+++ script.mjs
@@ -1,9 +1,13 @@
-import navbar from './navbar.mjs'
+import {navbar, getList} from './navbar.mjs'
+//paintings data
+import { paintingsArray } from './data.mjs';
 
 
 //run functions from modules
 navbar();
+getList();
 
 
 
+document.getElementById()
 
