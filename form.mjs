a1ceb362d6959cedd6c36a6470606e5a9a136d99
PsychoShaman <111572177+PsychoShaman@users.noreply.github.com>
2023-02-20 17:25:01 +0100

    add nodemailer

--- form.mjs
+++ form.mjs
@@ -1,5 +1,4 @@
-
-
+import nodemailer from 'nodemailer';
 function validateForm() {
     const form = document.getElementById("form");
     const name = document.getElementById("name");
@@ -31,13 +30,16 @@ function validateForm() {
 
 
 
-        if (messages.length > 0) 
-        {
+        if (messages.length > 0) {
             e.preventDefault();
             errorElement.innerText = messages.join(", ");
             errorElement.style.visibility = "visible";
 
+        }
 
+        if(messages.length === 0 ) 
+        {
+            sendEmail(name.value, email.value, subject.value, message.value);
             
         }
 
@@ -50,3 +52,33 @@ function validateForm() {
 export default validateForm();
 
 validateForm();
+
+
+function sendEmail(name, email, subject, message) {
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: 'dafirenze@wp.pl', // your email
+            pass: 'Zzx123nie' // your email account password
+        }
+    });
+
+
+    let mailOptions = {
+        from: '"Your Name" <your_email@example.com>', // sender address
+        to: 'recipient_email@example.com', // list of receivers
+        subject: 'Hello from Node.js', // Subject line
+        text: 'Hello world!', // plain text body
+        html: '<b>Hello world!</b>' // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+    });
+}
